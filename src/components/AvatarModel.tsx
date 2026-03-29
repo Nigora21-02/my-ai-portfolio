3559a243885af9b91d39166a90782f34ead5016b
2102kh <144224944+2102kh@users.noreply.github.com>
2026-03-29 21:21:17 +0200

    add customized avatar with animation support

--- src/components/AvatarModel.tsx
+++ src/components/AvatarModel.tsx
@@ -143,7 +143,7 @@ export default function AvatarModel({ isSpeaking, text }: AvatarProps) {
           <Visuals />
           <OfficeRoom />
           <Lampa />
-          <Avatar isSpeaking={isSpeaking} text={text} />
+          
         </Suspense>
       </Canvas>
     </div>
